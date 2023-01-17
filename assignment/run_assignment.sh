0c4e4db8eb067d1a40d8d2c1b4f3b42b92e9fba5
Owen Brooks <owen.h.brooks@gmail.com>
2023-01-17 17:41:30 +1100

    Rename check_results to check_matches

--- assignment/run_assignment.sh
+++ assignment/run_assignment.sh
@@ -1,8 +1,10 @@
 
 # make
-TIME=$({ time ./output/stats dataset_a/known dataset_a/unknown_no_transform > output/hashes.txt ; } 2>&1 >/dev/null)
-
 TIMEFORMAT='%3R'
-TIME=$({ time ./output/search dataset_a/known dataset_a/unknown_no_transform output/hashes.txt > output/results.txt ; } 2>&1 >/dev/null)
-python3 check_results.py image_prep/dataset_a_no_transform_answers.csv output/results.txt
-echo "Took $TIME seconds"
+
+# stats output is redirected to hashes.txt. stdout of time is ignored, but stderr of time is redirected to stdout out which is stored in STATS_TIME variable
+STATS_TIME=$({ time ./output/stats dataset_a/known dataset_a/unknown_no_transform > output/hashes.txt ; } 2>&1 >/dev/null)
+SEARCH_TIME=$({ time ./output/search dataset_a/known dataset_a/unknown_no_transform output/hashes.txt > output/matches.txt ; } 2>&1 >/dev/null)
+python3 check_matches.py image_prep/dataset_a_no_transform_answers.csv output/matches.txt
+echo "Stats took $STATS_TIME seconds."
+echo "Search took $SEARCH_TIME seconds."
